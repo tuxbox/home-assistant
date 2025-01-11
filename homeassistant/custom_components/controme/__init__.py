e0875f090beed8ca2192c629f6d41cb02894523f
Iulius Gutberlet <iulius@gutberlet.eu>
2025-01-11 21:15:24 +0100

    stuff

---
+++ homeassistant/custom_components/controme/__init__.py
@@ -0,0 +1,16 @@
+"""The Controme component."""
+
+# import logging
+#
+# from homeassistant.config_entries import ConfigEntry
+# from homeassistant.core import HomeAssistant
+#
+# _LOGGER = logging.getLogger(__name__)
+#
+# """Controme Home Assistant integration."""
+#
+#
+# async def async_setup_platform(hass: HomeAssistant, config: ConfigEntry) -> bool:
+#    """Set up the Controme component."""
+#    _LOGGER.info("Setting up Controme component")
+#    return True
---
+++ homeassistant/custom_components/controme/const.py
@@ -0,0 +1,3 @@
+"""Constants for the Controme integration."""
+
+DOMAIN = "controme"
